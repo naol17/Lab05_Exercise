2d536392451ad097e94f6d458c87acc5d4e34648
Naol Dame <naododa5566@gmail.com>
2021-01-29 22:39:13 +0300

    Creating event listners for bom.js

--- asset/js/boom.js
+++ asset/js/boom.js
@@ -23,3 +23,50 @@ const forhost = document.querySelector("#host");
 const forport = document.querySelector("#prt");
 
 const forhname = document.querySelector("#hname");
+
+// ui variables fro brows info
+const forappname = document.querySelector("#appname");
+const forversion = document.querySelector("#appver");
+const forplatF = document.querySelector("#platF");
+const forLanguage = document.querySelector("#lang");
+const forcooki = document.querySelector("#cooki");
+//screen info
+const forhei = document.querySelector("#hei");
+const forwid = document.querySelector("#wid");
+const forpx = document.querySelector("#pxdepth");
+// browsing history info
+const forBleng = document.querySelector("#Bleng");
+const forBstate = document.querySelector("#Bstate");
+//ui for screen info
+const forShight = document.querySelector('#hei');
+const forSwidth = document.querySelector('#wid');
+const forSpxd = document.querySelector('#pxdepth');
+//ui for browser history info
+
+const browserLength = document.querySelector('#Bleng');
+const browserState = document.querySelector('#Bstate');
+
+//event
+//for location
+forhref.addEventListener("click", show);
+forproto.addEventListener("click", sproto);
+forhost.addEventListener("click", shost);
+forport.addEventListener("click", sport);
+forhname.addEventListener("click", shostn);
+
+//event for browser info
+forappname.addEventListener('click', sappname)
+forversion.addEventListener('click', sversion)
+forplatF.addEventListener('click', splat)
+forLanguage.addEventListener('click', slangu)
+forcooki.addEventListener('click', scooki)
+
+//evevt for screen info
+
+forShight.addEventListener('click', shHight);
+forSwidth.addEventListener('click', shwidth);
+forSpxd.addEventListener('click', shPixle);
+
+//event for brows history info
+browserLength.addEventListener('click', bhistory);
+browserState.addEventListener('click', bstate);
