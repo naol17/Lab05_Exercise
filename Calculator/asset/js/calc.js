5df231a3a9f28b07c43b49de847429521066cf86
Naol Dame <naododa5566@gmail.com>
2021-01-31 21:40:08 +0300

    Creating constant variables

---
+++ Calculator/asset/js/calc.js
@@ -0,0 +1,7 @@
+// creating constant variables
+const numberbutton = document.querySelectorAll('.number')
+const operationbutton = document.querySelectorAll('.operator')
+const equalsbutton = document.querySelector('.equal')
+const deletbutton = document.querySelector('.delletall')
+const privelement = document.querySelector('.previousop')
+const curentelement = document.querySelector('.currentop')
